7df97700b3e57ab494ca3dc5bc8725a244632458
baby1938 <baby1938almaz@.com>
2023-08-14 14:26:43 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	for (i = 'a' ; i <= 'z' ; i++)
