48ffe606b2c1a32b78d928895b448e6af2168365
baby1938 <baby1938almaz@.com>
2023-08-14 13:52:07 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,12 +2,12 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/*betty style doc for function main goes there */
 /**
-* main - main function  
-*
-* Return: always 0
-*/
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int n;
