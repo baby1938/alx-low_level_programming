59ecc58db3b287f6427a5c1a120bda51292bef69
baby1938 <baby1938almaz@.com>
2023-08-15 15:08:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - print _putchar
- * 
+ * main - main function
+ *
  * Return: always 0
- */
+*/
 
 int main(void)
 {
