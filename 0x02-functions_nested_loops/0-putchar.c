a4d2a28578b953a8fdf2a2a7ebc5e34a630862da
baby1938 <baby1938almaz@.com>
2023-08-15 13:12:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ *_putchar - function that prints _putchar
+ * Return: returns 0;
+ */
+int main(void)
+{
+	_Putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
