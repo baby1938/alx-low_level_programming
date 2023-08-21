6a439a501ab6aec7a7c7506da0efde347b1b9658
baby1938 <baby1938almaz@.com>
2023-08-21 13:27:14 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by line, to stdout
+ * @str: string to print
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
